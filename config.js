350b69c3c1c1999823a91ad3ec3706ce10704c25
Jesse Chavez <jesse.chavez.r@gmail.com>
2017-04-20 07:30:21 +1000

    added oni config file

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "oni.useDefaultConfig": false,
+    "oni.loadInitVim": true,
+    "editor.fontSize": "14px",
+    "editor.fontFamily": "Droid Sans Mono Dotted for Powerline",
+    "editor.completions.enabled": true
+}
+
