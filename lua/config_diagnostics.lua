b019168b567ff42d9c476a91123045b78eb5836c
Jesse Chavez <jesse.chavez.r@gmail.com>
2022-07-17 09:04:54 +1000

    disable diagnostic auto open_float window (use E key instead)

--- lua/config_diagnostics.lua
+++ lua/config_diagnostics.lua
@@ -4,6 +4,7 @@
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
 -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', 'E', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[e', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']e', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
@@ -16,6 +17,10 @@ vim.diagnostic.config({
   underline = true,
   update_in_insert = true,
   severity_sort = false,
+  float = {
+    focus = false,
+    border = 'rounded'
+  }
 })
 
 local signs = { Error = '✗', Warn = '⚠', Hint = '‣', Info = '•' }
@@ -30,7 +35,7 @@ vim.opt.updatetime = 250 -- global refresh time
 
 -- opens diagnostic float window when cursor reach line
 -- (render on top of default float window when goto_prev or goto_next is used)
-vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(bor, {focus=false, border='rounded'})]]
+-- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(bor, {focus=false, border='rounded'})]]
 
 -- this does not seems to work
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
